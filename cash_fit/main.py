a2f8d556ad42b94b8d78896e721e91e9c6377a8d
change-challenge <dog941128@hotmail.com>
2022-04-17 02:29:50 +0900

    finish cashfit

--- cash_fit/main.py
+++ cash_fit/main.py
@@ -23,19 +23,45 @@ worksheet1 = import_googlesheet("현재 진행중")
 g_date = worksheet1.find(src.Time.c_f_time())
 
 def cash_fit_users():
-	daily_ts = client.conversations_history(channel=src.g_m_channel)["messages"][0]["ts"]
-	cash_fit_history = client.conversations_replies(channel=src.g_m_channel, ts=daily_ts)
+	daily_ts = client.conversations_history(channel=src.c_f_channel)["messages"][0]["ts"]
+	cash_fit_history = client.conversations_replies(channel=src.c_f_channel, ts=daily_ts)
 	for user in cash_fit_history["messages"]:
 		if (user["user"] == "U02D1UEL81Z"):
 			continue
 		c_f_users.append(src.UserID.users_id_to_name[user["user"]])
 	return (c_f_users)
 
-#print(worksheet1)
+def get_g_users(row, col):
+	return worksheet1.cell(row, col).value
+
+def send_cashfit():
+	print("===========send_cashfit()===========")
+	name_col = 2
+	date_col = g_date.col
+	date_row = g_date.row + 1
+	g_m_users = cash_fit_users()
+	flag = False
+	while (get_g_users(date_row, name_col) != None):
+		value = get_g_users(date_row, name_col)
+		check = get_g_users(date_row, date_col)
+		if (check == '-'):
+			date_row += 1
+			continue
+		for user in g_m_users:
+			if (user == value):
+				worksheet1.update_cell(date_row, date_col, 'O')
+				date_row += 1
+				flag = True
+				break
+		worksheet1.update_cell(date_row, date_col, 'X')
+		if (flag == True):
+			flag = False
+			continue
+		date_row += 1
 
 if __name__ == "__main__":
 	try:
-
+		send_cashfit()
 		client.chat_postMessage(channel=src.c_f_channel, blocks=src.c_f_state)
 		print("==============================")
 	except:
