f5b49b9f3a2eebac9e1c6e7a8232e4be69a6c941
change-challenge <dog941128@hotmail.com>
2022-04-16 23:19:04 +0900

    goodmorning

--- main.py
+++ main.py
@@ -1,30 +1,80 @@
+# 매일 0시 : 인증 글 올리기
+# 매일 8시 11분 : 기상 인증 체크
+# 매일 13시 : 오전 활동 인증 체크
+
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
 from oauth2client.service_account import ServiceAccountCredentials
+from datetime import date, datetime
 import gspread
+import src
+from slack import WebClient
+import os
+from pytz import timezone
 
-spreadsheet_url = "https://docs.google.com/spreadsheets/d/1doYowbfl1TJ3zn-QSGl8YMpr6P-0uUd_PPxrQGpAKLo/edit#gid=1934408757"
+today = datetime.now(timezone('Asia/Seoul'))
 
-hojin_key = "/Users/hojinjang/RichStudyProjectBot/config/goodmorningproject-657481cfc7b9.json"
-aws_key = "/home/ubuntu/RichStudyProjectBot/config/goodmorningproject-657481cfc7b9.json"
-json_key_path = hojin_key
-scope = [
-    "https://spreadsheets.google.com/feeds",
-    "https://www.googleapis.com/auth/drive",
-]
+g_m_users = []
+client = WebClient(src.bot_token)
+
+
+
+def get_ts():
+	daily_thread = client.conversations_history(channel=src.g_m_channel)["messages"][0]
+	return (daily_thread["ts"])
+daily_history = client.conversations_replies(channel=src.g_m_channel, ts=get_ts())
+
+for user in daily_history["messages"]:
+	if (user["user"] == "U02D1UEL81Z"):
+		continue
+	g_m_users.append(src.UserID.users_id_to_name[user["user"]])
 
 def import_googlesheet(tab):
-    scope = [
-        "https://spreadsheets.google.com/feeds",
-        "https://www.googleapis.com/auth/drive",
-    ]
-    credential = ServiceAccountCredentials.from_json_keyfile_name(json_key_path, scope)
+    credential = ServiceAccountCredentials.from_json_keyfile_name(src.json_key_path, src.scope)
     gc = gspread.authorize(credential)
-    doc = gc.open_by_url(spreadsheet_url)
+    doc = gc.open_by_url(src.spreadsheet_url)
     worksheet = doc.worksheet(tab)
     return (worksheet)
 
-worksheet1 = import_googlesheet('2기 : 2022년 2월28일-')
-#a = worksheet1.col_values(1)[3:]
-b = worksheet1.row_values(10)
-#print(a)
-print("==============================")
-print(b)
+worksheet1 = import_googlesheet("4/18~5/15")
+g_date = worksheet1.find(src.Time.g_m_time())
+
+def get_g_users(row, col):
+	return worksheet1.cell(row, col).value
+
+def send_morning():
+	name_col = 1
+	date_col = g_date.col
+	date_row = g_date.row + 1
+	while (get_g_users(date_row, name_col) != None):
+		value = get_g_users(date_row, name_col)
+		for user in g_m_users:
+			if (user == value):
+				worksheet1.update_cell(date_row, date_col, 'O')
+				date_row += 1
+				break
+		worksheet1.update_cell(date_row, date_col, 'X')
+		print(value)
+		date_row += 1
+
+
+
+if __name__ == "__main__":
+	try:
+		if (today.hour == 23):
+			client.chat_postMessage(channel=src.g_m_channel, blocks=src.g_m_state)
+		if (today.hour == 23):
+			send_morning()
+		if (today.hour == 13):
+			send_morning()
+		print("==============================")
+	except:
+		print("오류가 발생하였습니다.")
+
+
+
+
+
+#print(b)
+#a = worksheet1.col_values(2)[3:]
+#b = worksheet1.row_values(10)
---
+++ src.py
@@ -0,0 +1,92 @@
+from datetime import datetime
+from pytz import timezone
+import os
+
+g_m_channel = "C03A5GVDQ9G"
+bot_token = "xoxb-1830334374899-2443966688067-uYiR4dWVtj11GqJvE6D6ykBg"
+spreadsheet_url = "https://docs.google.com/spreadsheets/d/1doYowbfl1TJ3zn-QSGl8YMpr6P-0uUd_PPxrQGpAKLo/edit#gid=1934408757"
+
+cur_dir = os.getcwd()
+json_key_path = cur_dir + "/config/goodmorningproject-657481cfc7b9.json"
+
+scope = [
+    "https://spreadsheets.google.com/feeds",
+    "https://www.googleapis.com/auth/drive",
+]
+
+class Time:
+	def g_m_post_time():
+			return (datetime.now(timezone('Asia/Seoul'))).strftime('%Y년 %-m월 %-d일')
+	def g_m_time():
+		return (datetime.now(timezone('Asia/Seoul'))).strftime('%-m/%-d')
+
+g_m_state = [
+	{
+		"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "*[굿모닝🌞프로젝트 인증 - " + Time.g_m_post_time() + "]* \n\n"
+			}
+	}
+]
+
+class UserID:
+	users_name = [
+				"강동운",
+				"김가현",
+				"김민수",
+				"김범태",
+				"김성훈",
+				"김주영",
+				"문건호",
+				"박상아",
+				"박상진",
+				"박서지",
+				"박재형",
+				"박혜빈",
+				"서수희",
+				"신환종",
+				"심혜빈",
+				"양희재",
+				"윤영주",
+				"이재원",
+				"이현정",
+				"장호진",
+				"전진표",
+				"정하진",
+				"정현진",
+				"천지영",
+				"최명원",
+				"하승희",
+				"한수경"
+			]
+	
+	users_id_to_name = {
+				"U02RL2XV4KH" : "강동운",
+				"U02RTQ2NXKQ" : "김가현",
+				"U02LE7HKCE6" : "김민수",
+				"U027JQK00KB" : "김범태",
+				"U01QT3KFU4D" : "김성훈",
+				"U026RFKEN3H" : "김주영",
+				"U02733TKY9K" : "문건호",
+				"U02S33JRS9J" : "박상아",
+				"U02S4TERJ5S" : "박상진",
+				"U02S2LM4LP4" : "박서지",
+				"U02S0M72N82" : "박재형",
+				"U01QMTD1K37" : "박혜빈",
+				"U02S2PVS3B6" : "서수희",
+				"U01QSTF6M18" : "신환종",
+				"U02RU9NTCA2" : "심혜빈",
+				"U01QEPH2TP1" : "양희재",
+				"U01QLRF7MJ6" : "윤영주",
+				"U02AKLMDSRE" : "이재원",
+				"U02RTS9357G" : "이현정",
+				"U01R3U5FQL8" : "장호진",
+				"U02S3UAPS9J" : "전진표",
+				"U02SR22Q9MW" : "정하진",
+				"U02727PASNS" : "정현진",
+				"U032CUPKA95" : "천지영",
+				"U02S0ER3W9H" : "최명원",
+				"U02RU5T3KSS" : "하승희",
+				"U027382N5K7" : "한수경"
+			}
