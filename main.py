bdaaac39d4578712219c5f6dc4a67c408d46a13f
change-challenge <dog941128@hotmail.com>
2022-04-17 00:38:07 +0900

    finish / just before clean up

--- main.py
+++ main.py
@@ -5,27 +5,18 @@
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 from oauth2client.service_account import ServiceAccountCredentials
-from datetime import date, datetime
+from datetime import datetime, timedelta
 import gspread
 import src
 from slack import WebClient
-import os
+import time
 from pytz import timezone
 
 today = datetime.now(timezone('Asia/Seoul'))
 
 g_m_users = []
+g_m_active_users = []
 client = WebClient(src.bot_token)
-
-def get_ts():
-	daily_thread = client.conversations_history(channel=src.g_m_channel)["messages"][0]
-	return (daily_thread["ts"])
-daily_history = client.conversations_replies(channel=src.g_m_channel, ts=get_ts())
-
-for user in daily_history["messages"]:
-	if (user["user"] == "U02D1UEL81Z"):
-		continue
-	g_m_users.append(src.UserID.users_id_to_name[user["user"]])
 
 def import_googlesheet(tab):
     credential = ServiceAccountCredentials.from_json_keyfile_name(src.json_key_path, src.scope)
@@ -34,19 +25,41 @@ def import_googlesheet(tab):
     worksheet = doc.worksheet(tab)
     return (worksheet)
 
-worksheet1 = import_googlesheet("4/18~5/15")
+worksheet1 = import_googlesheet("현재 진행중")
 g_date = worksheet1.find(src.Time.g_m_time())
+
+def get_ts():
+	daily_thread = client.conversations_history(channel=src.g_m_channel)["messages"][0]
+	return (daily_thread["ts"])
+
+def morning_users():	
+	morming_history = client.conversations_replies(channel=src.g_m_channel, ts=get_ts())
+	for user in morming_history["messages"]:
+		if (user["user"] == "U02D1UEL81Z"):
+			continue
+		g_m_users.append(src.UserID.users_id_to_name[user["user"]])
+	return (g_m_users)
+
+def morning_active_users():
+	morning_time = time.mktime((today - timedelta(hours=5)).timetuple())
+	morming_active_history = client.conversations_replies(channel=src.g_m_channel, ts=get_ts(), oldest=morning_time)
+	for user in morming_active_history["messages"]:
+		if (user["user"] == "U02D1UEL81Z"):
+			continue
+		g_m_active_users.append(src.UserID.users_id_to_name[user["user"]])
+	return (g_m_active_users)
 
 def get_g_users(row, col):
 	return worksheet1.cell(row, col).value
 
 def send_morning():
+	print("===========send_morning()===========")
 	name_col = 1
 	date_col = g_date.col
 	date_row = g_date.row + 1
 	while (get_g_users(date_row, name_col) != None):
 		value = get_g_users(date_row, name_col)
-		for user in g_m_users:
+		for user in morning_users():
 			if (user == value):
 				worksheet1.update_cell(date_row, date_col, 'O')
 				date_row += 1
@@ -55,24 +68,29 @@ def send_morning():
 		print(value)
 		date_row += 1
 
-
+def send_morning_active():
+	print("===========send_morning_active()===========")
+	name_col = 1
+	date_col = g_date.col
+	date_row = g_date.row + 1
+	while (get_g_users(date_row, name_col) != None):
+		value = get_g_users(date_row, name_col)
+		for user in morning_active_users():
+			if (user == value):
+				worksheet1.update_cell(date_row, date_col, 'O/O')
+				date_row += 1
+				break
+		print(value)
+		date_row += 1
 
 if __name__ == "__main__":
 	try:
-		if (today.hour == 23):
+		if (today.hour == 0):
 			client.chat_postMessage(channel=src.g_m_channel, blocks=src.g_m_state)
-		if (today.hour == 23):
+		if (today.hour == 8):
 			send_morning()
 		if (today.hour == 13):
-			send_morning()
+			send_morning_active()
 		print("==============================")
 	except:
 		print("오류가 발생하였습니다.")
-
-
-
-
-
-#print(b)
-#a = worksheet1.col_values(2)[3:]
-#b = worksheet1.row_values(10)
